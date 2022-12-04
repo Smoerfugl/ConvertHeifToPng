2c15c00245aed383bbe898285a5d7a6df34fa877
Chris Justesen <chris@smoerfugl.dk>
2022-12-04 16:10:09 +0100

    Remove files no longer being used

--- publish.sh
+++
@@ -1,9 +0,0 @@
-dotnet publish \
-        -c Release \
-        -o publish \
-        -p:PublishReadyToRun=true \
-        -p:PublishSingleFile=true \
-        -p:PublishTrimmed=true \
-        --self-contained=true \
-        -p:IncludeNativeLibrariesForSelfExtract=true
-
